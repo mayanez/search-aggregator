00b67aada0d63932a438dd05e26111b3b50e5fb6
Miguel Yanez <miguel.themissinglink@gmail.com>
2014-10-18 12:57:05 -0400

    Adds Initial ixirc scraper.

---
+++ scrapers/ixircScraper.py
@@ -0,0 +1,70 @@
+import requests
+import urllib
+import XDCCFile
+from bs4 import BeautifulSoup
+
+class ixircScraper(object):
+	def __init__(self):
+		self.url = "http://ixirc.com"
+		
+		self.params = {
+			"q" : "",
+			"pn" : "0"
+		}
+
+	def search(self, query, pages=2):
+		self.params["q"] = query
+		results = list()
+
+		max_pages = self.__maxPages(query)
+
+		for page in xrange(pages):
+			if (page <= max_pages):
+				results += self.__search(query, page)
+			else:
+				break
+
+		return results
+
+	def __search(self, query, page):
+		self.params["q"] = query
+		self.params["pn"] = page
+		response = requests.get(self.url, params=self.params)
+		soup = BeautifulSoup(response.text)
+
+		xdcc_file_list = list()
+
+		results_table = soup.find("table", {"id": "results-table"})
+
+		file_list = results_table.findAll("tr", {"class" : ["even", "odd"]})
+
+		for a_file in file_list:
+			file_info = a_file.findAll("td")
+			name = file_info[0].get_text()
+			network = file_info[1].get_text()
+			channel = file_info[2].get_text()
+			user = file_info[3].get_text()
+			number = file_info[4].get_text()
+			size = file_info[6].get_text()
+			xdcc_file = XDCCFile.XDCCFile(name, network, channel, user, number, size)
+			xdcc_file_list.append(xdcc_file)
+
+		return xdcc_file_list
+
+	def __maxPages(self, query):
+
+		self.params["q"] = query
+		self.params["pn"] = 0
+		response = requests.get(self.url, params=self.params)
+		soup = BeautifulSoup(response.text)
+
+		page_numbers = soup.find("div", {"class" : "page-numbers"})
+		max_pages = page_numbers.findAll("a")[-1].get_text()
+		
+		return int(max_pages)
+
+if __name__ == '__main__':
+	scraper = ixircScraper()
+	results = scraper.search("hi")
+	print results[0]
+
